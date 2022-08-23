e700eb3be4b64c21da488f578926433dd3f97850
Atefeh Masoumi <atefeh.masoomy@gmail.com>
2022-08-23 17:00:30 +0430

    adding some data for test before api

--- src/data.js
+++ src/data.js
@@ -1,9 +1,16 @@
-const Prouducts =[
+export const Prouducts =[
     {
         id:1,
         category: "ghalami",
-        name:"باتری قلمی شارژی وارتا 2100mAh بسته 2 عددی",
-        img:""
+        namepersian:"باتری قلمی شارژی وارتا 2100mAh بسته 2 عددی",
+        img:[
+            "./asset/images/backend-temprory-photos/ghalami/AA-2100-1-.webp",
+            "./asset/images/backend-temprory-photos/ghalami/AA-2100-2.webp",
+            "./asset/images/backend-temprory-photos/ghalami/AA-2100-3.webp",
+        ],
+        tag:[
+            "toy","torch","camera","charging"
+        ],
 
     
     },
