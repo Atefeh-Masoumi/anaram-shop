c066e8c9fa3f6e84ecc72d4c393050e0fec93f48
Atefeh Masoumi <atefeh.masoomy@gmail.com>
2022-08-30 17:02:50 +0430

    product header

--- src/components/ProductHeader/ProductHeader.js
+++ src/components/ProductHeader/ProductHeader.js
@@ -7,10 +7,18 @@ const ProductHeader = () => {
     return ( 
         <div>
             <div className="position-relative">
-                <img src={headimage}  alt="Header-free-shipping" className="position-relative Header-img"/>
-                <div>
-                    <div>
-                        <span className="productheader-text bg-color position-absolute">ارسال رایگان</span>
+                <div className="position-relative">
+                <img src={headimage}  alt="Header-free-shipping" className=" Header-img"/>
+                </div>
+                <div className="d-flex bg-black position-absolute">
+                    <div  className="text-align-center mb-4">
+                        <span className="productheader-text ">ارسال رایگان</span>
+                    </div>
+                    <div className="text-align-center mb-4">
+                        <span className="productheader-text2">به سراسر کشور </span>
+                    </div>
+                    <div  className="text-align-center mb-4">
+                        <span className="productheader-text3">  خریدهای بالای 250 هزار تومان </span>
                     </div>
 
                 </div>
